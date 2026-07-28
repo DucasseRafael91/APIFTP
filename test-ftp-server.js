5a19ee26ccbd71a9d1eaed612f40d6a0993acf42
Ducasse Rafael <rafael.ducasse@fms-ea.com>
2026-07-28 14:23:32 +0200

    API relais upload FTP

--- test-ftp-server.js
+++
@@ -1,9 +0,0 @@
-const FtpSrv = require('ftp-srv');
-const ftpServer = new FtpSrv({
-  url: 'ftp://127.0.0.1:2121',
-  pasv_range: '2122-2130',
-  pasv_url: '127.0.0.1'
-});
-
-ftpServer.on('login', (data, resolve) => resolve({ root: __dirname + '/test-ftp-storage' }));
-ftpServer.listen().then(() => console.log('Faux serveur FTP de test sur le port 2121'));
